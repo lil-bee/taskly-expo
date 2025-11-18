009b342a6140c3f21210bd2eaae744fc6dc265c9
lil-bee <fadhilakbar.2019@student.uny.ac.id>
2025-11-18 22:39:32 +0700

    ✔️ Example of presentation modal

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,14 @@ export default function Layout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: "Shopping List" }} />
+      <Stack.Screen
+        name="counter"
+        options={{ title: "counter coi", presentation: "modal" }}
+      />
+      <Stack.Screen
+        name="idea"
+        options={{ title: "idea coi", presentation: "modal" }}
+      />
     </Stack>
   );
 }
