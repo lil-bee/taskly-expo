1d0da91d19385bc1d6196e5aa2ee0a485fc6e523
lil-bee <fadhilakbar.2019@student.uny.ac.id>
2025-11-25 21:37:44 +0700

    ✔️ Show past completions on the history page

--- app/counter/history.tsx
+++ app/counter/history.tsx
@@ -1,20 +1,68 @@
-import { StyleSheet, Text, View } from "react-native";
+import { format } from "date-fns";
+import { useEffect, useState } from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { countdownStorageKey, PersistedCountdownState } from ".";
+import { theme } from "../../theme";
+import { getFromStorage } from "../../utils/storage";
+
+const fullDateFormat = "LLL d yyyy, h:mm aaa";
 
 export default function HistoryScreen() {
+  const [countdownState, setCountdownState] =
+    useState<PersistedCountdownState>();
+
+  useEffect(() => {
+    const init = async () => {
+      const value = await getFromStorage(countdownStorageKey);
+      setCountdownState(value);
+    };
+
+    init();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>History</Text>
-    </View>
+    <FlatList
+      style={styles.list}
+      contentContainerStyle={styles.contentContainer}
+      data={countdownState?.completedAtTimestamps}
+      renderItem={({ item }) => (
+        <View style={styles.listItem}>
+          <Text style={styles.listItemText}>
+            {format(item, fullDateFormat)}
+          </Text>
+        </View>
+      )}
+      ListEmptyComponent={
+        <View style={styles.listEmpyContainer}>
+          <Text>Your shopping list is empty</Text>
+        </View>
+      }
+    />
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  list: {
     flex: 1,
+    backgroundColor: theme.colorWhite,
+  },
+  contentContainer: {
+    marginTop: 8,
+  },
+  listItem: {
+    alignItems: "center",
+    marginHorizontal: 8,
+    marginBottom: 8,
+    backgroundColor: theme.colorLightGrey,
+    padding: 12,
+    borderRadius: 6,
+  },
+  listItemText: {
+    fontSize: 18,
+  },
+  listEmpyContainer: {
     justifyContent: "center",
     alignItems: "center",
-  },
-  text: {
-    fontSize: 24,
+    marginVertical: 18,
   },
 });
